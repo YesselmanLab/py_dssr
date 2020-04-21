9c330ff2fed07cfb8c57040e644e90e071d00427
Joe Yesselman <jyesselm@unl.edu>
2020-04-21 11:22:11 -0500

    minor class reorangization

--- py_dssr/parser.py
+++ py_dssr/parser.py
@@ -70,24 +70,38 @@ def setup_pair_class(pair_data):
     return pair_str
 
 
+def default_class_setup(data, name):
+    items = data[0].items()
+    attrib_str = get_attribute_str(items)
+    nt_str = class_str.format(name=name, attributes=attrib_str, args="")
+    return nt_str
+
+
+
 def setup_dssr_classes():
     # get nt members
     exe_path = "resources/x3dna-dssr "
     pdb_path = "resources/4p95.pdb"
-    json_str = subprocess.check_output(exe_path + "-i=" + pdb_path + " --json --more", shell=True)
-    cleanup()
 
-    data = json.loads(json_str)
+    data = dssr.get_dssr_json_output(exe_path, pdb_path)
 
-    #pprint.pprint(data['pairs'])
+    pprint.pprint(data['splayUnits'])
     #print(data['atom2bases'][0])
 
     nt_str = setup_nt_class(data['nts'])
     pair_str = setup_pair_class(data['pairs'])
+    hairpin_str = default_class_setup(data['hairpins'], "DSSR_HAIRPIN")
 
-    f = open("dssr.py", "w")
+    f = open("dssr_classes.py", "w")
+    f.write("from typing import List, Dict\n\n")
     f.write(nt_str + "\n")
     f.write(pair_str + "\n")
+
+    keys = "hairpins,helices,stems,iloops,junctions,ssSegments,kissingLoops,Aminors,riboseZippers,HtypePknots,hbonds".split(",")
+    class_names = "HAIRPIN,HELIX,STEM,ILOOP,JUNCTION,SINGLE_STRAND,KISSING_LOOP,AMINOR,RIBOSE_ZIPPER,PSEUDOKNOT,HBOND".split(",")
+
+    for key, class_name in zip(keys,class_names):
+        f.write(default_class_setup(data[key],"DSSR_"+class_name) + "\n")
     f.close()
 
 
@@ -95,91 +109,18 @@ def setup_dssr_classes():
     #    print("self." + key + "=None")
 
 
-"""class DSSROutput(object):
-    def __init__(self):
-        pass
-
-
-class DSSR_NT(object):
-    def __init__(self, **kwargs):
-        self.index = None
-        self.index_chain = None
-        self.chain_name = None
-        self.nt_resnum = None
-        self.nt_name = None
-        self.nt_code = None
-        self.nt_id = None
-        self.dbn = None
-        self.summary = None
-        self.alpha = None
-        self.beta = None
-        self.gamma = None
-        self.delta = None
-        self.epsilon = None
-        self.zeta = None
-        self.epsilon_zeta = None
-        self.bb_type = None
-        self.chi = None
-        self.glyco_bond = None
-        self.C5prime_xyz = None
-        self.P_xyz = None
-        self.form = None
-        self.ssZp = None
-        self.Dp = None
-        self.splay_angle = None
-        self.splay_distance = None
-        self.splay_ratio = None
-        self.eta = None
-        self.theta = None
-        self.eta_prime = None
-        self.theta_prime = None
-        self.eta_base = None
-        self.theta_base = None
-        self.v0 = None
-        self.v1 = None
-        self.v2 = None
-        self.v3 = None
-        self.v4 = None
-        self.amplitude = None
-        self.phase_angle = None
-        self.puckering = None
-        self.sugar_class = None
-        self.bin = None
-        self.cluster = None
-        self.suiteness = None
-        self.filter_rmsd = None
-        self.frame = None
-        self.is_broken = None
-
-        for key, value in kwargs.items():
-            setattr(self, key, value)
-
-
-class DSSR_PAIR(object):
-    def __init__(self, **kwargs):
-        pass
-"""
-
-def cleanup():
-    files = glob.glob("dssr-*")
-    for f in files:
-        os.remove(f)
 
 
 def main():
     exe_path = "resources/x3dna-dssr "
     pdb_path = "/Users/josephyesselman/projects/Rosetta.projects/motif_folding/runs/fixed_ends/TWOWAY.1S72.47/S_000001.pdb"
+    #pdb_path = "resources/4p95.pdb"
 
-    #setup_dssr_classes()
+    setup_dssr_classes()
 
-    #exit()
+    exit()
 
-    # need to call analyze first
-    ##subprocess.call(exe_path + "-i=" + pdb_path + " --analyze", shell=True)
-    #print(exe_path + "-i=" + pdb_path + " --json --more ")
-    json_str = subprocess.check_output(exe_path + "-i=" + pdb_path + " --json --more", shell=True)
-    cleanup()
-    data = json.loads(json_str)
+    data = dssr.get_dssr_json_output(exe_path, pdb_path)
 
     #print(data['pairs'])
 
@@ -187,6 +128,9 @@ def main():
     for nt_info in data['nts']:
         nt = dssr.DSSR_NT(**nt_info)
         nts[nt.nt_id] = nt
+
+    for nt in nts.values():
+        print(nt.nt_id, nt.splay_angle, nt.splay_distance)
 
     pairs = []
 
@@ -196,6 +140,9 @@ def main():
         del pair_info['nt1']
         del pair_info['nt2']
         pairs.append(dssr.DSSR_PAIR(nt1, nt2, **pair_info))
+
+    #m = dssr.DSSR_MOTIF(pairs)
+
 
     #print(data['refCoords'])
 
